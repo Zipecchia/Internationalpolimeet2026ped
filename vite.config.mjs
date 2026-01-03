da575edde35a5b59ee623d3d130a4f333cbb2f21
Zipecchia <viola.naldi5@gmail.com>
2026-01-03 02:22:05 +0100

    new

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import path from "node:path"
 
 export default defineConfig({
   plugins: [react()],
-
-  // IMPORTANTISSIMO per GitHub Pages su repo:
   base: "/Internationalpolimeet2026ped/",
-
-  resolve: {
-    alias: {
-      "@": path.resolve(process.cwd(), "src"),
-    },
-  },
-
   build: {
     outDir: "dist",
   },
